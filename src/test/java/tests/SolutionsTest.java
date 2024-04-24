200ae10515bb30e8b111e6305612c93ae0e39682
AOvsyannikov <aovsyannikov@it-one.ru>
2024-04-24 21:39:41 +0300

    added .SolutionsTest.java

---
+++ src/test/java/tests/SolutionsTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
+
+public class SolutionsTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void solutions_test(){
+
+        open("https://github.com/");
+        $(byTagAndText("button", "Solutions")).hover();
+        $(byTagAndText("a", "Enterprise")).click();
+        webdriver().shouldHave(url("https://github.com/enterprise"));
+
+    }
+}
