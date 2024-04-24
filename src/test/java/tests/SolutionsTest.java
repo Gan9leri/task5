28911cbd86687019ec8d54c705db19da78b24818
AOvsyannikov <aovsyannikov@it-one.ru>
2024-04-24 21:41:16 +0300

    changed .SolutionsTest.java

--- src/test/java/tests/SolutionsTest.java
+++ src/test/java/tests/SolutionsTest.java
@@ -1,3 +1,8 @@
+/*На главной странице GitHub выберите меню Solutions -> Enterprize
+с помощью команды hover для Solutions. Убедитесь что загрузилась нужная страница
+(например что заголовок - "Build like the best."
+ */
+
 package tests;
 
 import com.codeborne.selenide.Configuration;
